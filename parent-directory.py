5d5dc5a8861e30bcbfacfb7418eff31c39b2a31c
Chilton3409 <nate.chilton123@gmail.com>
2021-03-25 15:31:26 -0500

    returns the name of the directory thats located just above the current working directoy

---
+++ parent-directory.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import os
+def parent_directory():
+    #Create a relative path to the parent
+    #of the current working directory
+    relative_parent = os.path.join(os.getcwd(),'..')
+
+    #return the absolute path of the parent directory
+    return os.path.abspath(relative_parent)
+
+print(parent_directory())
