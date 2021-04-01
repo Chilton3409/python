d06a49711ce444ec2f405ddc5b0ec0ecc3df30bb
Chilton3409 <nate.chilton123@gmail.com>
2021-04-01 10:27:34 -0500

    changed content of the text variable

--- text_to_speech.py
+++ text_to_speech.py
@@ -3,6 +3,6 @@ import os
 from gtts import gTTS
 cwd = os.getcwd()
 os.mkdir('mp3')
-text = "Hello Matt how is your christmas?"
+text = "Hello, how are you today?"
 tts = gTTS(text)
 tts.save("mp3/hi.mp3")
