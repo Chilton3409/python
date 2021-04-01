b8d2fec8647fb495c6e0dc58642ed1f481caf8c4
Chilton3409 <nate.chilton123@gmail.com>
2021-04-01 10:21:40 -0500

    changed savefile location to a new mp3 directory

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-
+import os
 from gtts import gTTS
-
+cwd = os.getcwd()
+os.mkdir('mp3')
 text = "Hello Matt how is your christmas?"
 tts = gTTS(text)
-tts.save("/Users/nathanchilton/scripts/hi.mp3")
+tts.save("mp3/hi.mp3")
